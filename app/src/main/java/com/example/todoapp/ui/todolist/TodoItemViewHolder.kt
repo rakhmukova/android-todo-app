1575fef3b1e3d258def46c8d7f476a6e902501ab
rakhmukova <lessness19@gmail.com>
2023-06-23 17:20:39 +0300

    Display date in todo item

--- app/src/main/java/com/example/todoapp/ui/todolist/TodoItemViewHolder.kt
+++ app/src/main/java/com/example/todoapp/ui/todolist/TodoItemViewHolder.kt
@@ -1,8 +1,10 @@
 package com.example.todoapp.ui.todolist
 
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todoapp.data.model.TodoItem
 import com.example.todoapp.databinding.TodoItemBinding
+import com.example.todoapp.util.DateParser
 
 class TodoItemViewHolder(
     private val binding: TodoItemBinding,
@@ -12,6 +14,7 @@ class TodoItemViewHolder(
     fun onBind(todoItem: TodoItem){
         binding.isDoneRadioButton.isChecked = todoItem.isCompleted
         binding.taskText.text = todoItem.text
+        binding.taskDeadline.text = DateParser.parse(todoItem.deadline)
 
         binding.isDoneRadioButton.setOnCheckedChangeListener { _, isChecked ->
             callbacks.onTodoItemCheckedChanged(todoItem, isChecked)
