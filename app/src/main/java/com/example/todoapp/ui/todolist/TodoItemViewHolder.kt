42098a468fe97f7353ce2dc2f5729a37c2615c3b
rakhmukova <lessness19@gmail.com>
2023-06-25 14:26:28 +0300

    Cross task description when task is done

--- app/src/main/java/com/example/todoapp/ui/todolist/TodoItemViewHolder.kt
+++ app/src/main/java/com/example/todoapp/ui/todolist/TodoItemViewHolder.kt
@@ -1,6 +1,7 @@
 package com.example.todoapp.ui.todolist
 
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todoapp.R
 import com.example.todoapp.data.model.Priority
@@ -14,17 +15,20 @@ class TodoItemViewHolder(
     ) : RecyclerView.ViewHolder(binding.root) {
 
     fun onBind(todoItem: TodoItem){
-        binding.isCompletedCheckBox.isChecked = todoItem.isCompleted
-        binding.taskText.text = todoItem.text
-        binding.taskDeadline.text = DateParser.parse(todoItem.deadline)
+        setTaskDeadline(todoItem)
+        setTaskDescription(todoItem)
         setPriorityIcon(todoItem)
+        setCheckBox(todoItem)
+    }
 
+    private fun setTaskDeadline(todoItem: TodoItem) {
+        binding.taskDeadline.text = DateParser.parse(todoItem.deadline)
+    }
+
+    private fun setCheckBox(todoItem: TodoItem) {
+        binding.isCompletedCheckBox.isChecked = todoItem.isCompleted
         binding.isCompletedCheckBox.setOnCheckedChangeListener { _, isChecked ->
             callbacks.onTodoItemCheckedChanged(todoItem, isChecked)
-        }
-
-        binding.taskText.setOnClickListener {
-            callbacks.onTodoItemClicked(todoItem)
         }
     }
 
@@ -41,6 +45,21 @@ class TodoItemViewHolder(
             Priority.COMMON -> {
                 binding.priorityIcon.visibility = View.GONE
             }
+        }
+    }
+
+    private fun setTaskDescription(todoItem: TodoItem) {
+        binding.taskText.text = todoItem.text
+        if (todoItem.isCompleted) {
+            binding.taskText.paint.isStrikeThruText = true
+            binding.taskText.setTextColor(ContextCompat.getColor(binding.root.context, R.color.label_tertiary))
+        } else {
+            binding.taskText.paint.isStrikeThruText = false
+            binding.taskText.setTextColor(ContextCompat.getColor(binding.root.context, R.color.label_primary))
+        }
+
+        binding.taskText.setOnClickListener {
+            callbacks.onTodoItemClicked(todoItem)
         }
     }
 }
