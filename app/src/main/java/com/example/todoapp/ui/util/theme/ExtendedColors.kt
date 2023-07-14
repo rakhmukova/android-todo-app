d10f0137aec7682e2ea20953b19fa53771f25fd0
rakhmukova <lessness19@gmail.com>
2023-07-14 19:40:45 +0200

    Create compose theme

---
+++ app/src/main/java/com/example/todoapp/ui/util/theme/ExtendedColors.kt
@@ -0,0 +1,46 @@
+package com.example.todoapp.ui.util.theme
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Color
+
+@Immutable
+data class ExtendedColors(
+    val supportSeparator: Color = Color.Unspecified,
+    val supportOverlay: Color = Color.Unspecified,
+    val labelPrimary: Color = Color.Unspecified,
+    val labelSecondary: Color = Color.Unspecified,
+    val labelTertiary: Color = Color.Unspecified,
+    val labelDisable: Color = Color.Unspecified,
+    val backPrimary: Color = Color.Unspecified,
+    val backSecondary: Color = Color.Unspecified,
+    val backElevated: Color = Color.Unspecified
+)
+
+val lightExtendedColors = ExtendedColors(
+    supportSeparator = LightSupportSeparator,
+    supportOverlay = LightSupportOverlay,
+    labelPrimary = LightLabelPrimary,
+    labelSecondary = LightLabelSecondary,
+    labelTertiary = LightLabelTertiary,
+    labelDisable = LightLabelDisable,
+    backPrimary = LightBackPrimary,
+    backSecondary = LightBackSecondary,
+    backElevated = LightBackElevated
+)
+
+val darkExtendedColors = ExtendedColors(
+    supportSeparator = DarkSupportSeparator,
+    supportOverlay = DarkSupportOverlay,
+    labelPrimary = DarkLabelPrimary,
+    labelSecondary = DarkLabelSecondary,
+    labelTertiary = DarkLabelTertiary,
+    labelDisable = DarkLabelDisable,
+    backPrimary = DarkBackPrimary,
+    backSecondary = DarkBackSecondary,
+    backElevated = DarkBackElevated
+)
+
+val LocalExtendedColors = staticCompositionLocalOf {
+    ExtendedColors()
+}
